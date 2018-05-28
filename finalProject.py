95ab2c68ed70b82dcd6752afbc556e317ba00a0c
vagrant <vagrant@vagrant.vm>
2018-05-28 21:51:20 +0000

    Fix bug in create new restaurant. Adding multiple restaurants makes all new restaurants the same. All methods *finished* except deleteMenuItem.

--- finalProject.py
+++ finalProject.py
@@ -11,8 +11,7 @@ restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', '
 
 #Fake Menu Items
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1', 'restaurant_id': '1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2', 'restaurant_id':'1'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3', 'restaurant_id':'2'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4', 'restaurant_id':'3'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5', 'restaurant_id':'1'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree', 'restaurant_id': '1'}
-
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree', 'id': '1', 'restaurant_id': '1'}
 
 
 #/restaurants page 
@@ -26,9 +25,10 @@ def showRestaurants():
 @app.route('/restaurants/new/', methods=['GET', 'POST'])
 def newRestaurant():
 	if request.method == 'POST':
-		restaurant['name'] = request.form['name']
-		restaurant['id'] = str(int(restaurants[len(restaurants)-1]['id']) + 1)
-		restaurants.append(restaurant)
+		restaurantCopy = restaurant.copy()
+		restaurantCopy['name'] = request.form['name']
+		restaurantCopy['id'] = str(int(restaurants[len(restaurants)-1]['id']) + 1)
+		restaurants.append(restaurantCopy)
 		return redirect(url_for('showRestaurants'))
 	else: 
 		return render_template('newrestaurant.html')
@@ -81,7 +81,21 @@ def showMenu(restaurant_id):
 #/restaurants/restaurant_id/menu/new
 @app.route('/restaurants/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-	return "This page is for making a new menu item for restaurant %s" % restaurant_id
+	if request.method == 'POST':
+		itemCopy = item.copy()
+		itemCopy['name'] = request.form['name']
+		itemCopy['description'] = request.form['description']
+		itemCopy['price'] = request.form['price']
+		itemCopy['course'] = request.form.get('course')
+		itemCopy['id'] = str(len(items)+1)
+		itemCopy['restaurant_id'] = str(restaurant_id)
+		items.append(itemCopy)
+		for i in items: 
+			print i['id'] + i['name'] + ", "
+
+		return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 #/restaurants/restaurant_id/menu/menu_id/edit 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
@@ -96,7 +110,6 @@ def editMenuItem(restaurant_id, menu_id):
 				items[i]['name'] = request.form['name']
 				items[i]['description'] = request.form['description']
 				items[i]['price'] = request.form['price']
-
 				items[i]['course'] = request.form.get('course')
 				return redirect(url_for('showMenu', restaurant_id=restaurant_id))
 	else:			
@@ -105,7 +118,18 @@ def editMenuItem(restaurant_id, menu_id):
 #/restaurants/restaurant_id/menu/menu_id/new
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/new/', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-	return "This page is for deleting menu item {0} in restaurant {1}".format(menu_id, restaurant_id)
+	for i in range(len(items)): 
+		if items[i]['restaurant_id'] == str(restaurant_id) and items[i]['id'] == str(menu_id):
+			currentItem = items[i];
+			print str(currentItem)
+
+	if request.method == 'POST':
+		for i in range(len(items)): 
+			if items[i]['restaurant_id'] == str(restaurant_id) and items[i]['id'] == str(menu_id):
+				items.pop(i)
+				return redirect(url_for('showMenu', restaurant_id=restaurant_id, menu_id=menu_id))
+	else: 
+		return render_template('deletemenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=currentItem)
 
 
 if __name__ == '__main__':
